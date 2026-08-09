87db219393fb52258bc41a793b64d9ad416850d6
‪Aqel Mohammed‬‏ <aqelmoh77@gmail.com>
2026-08-09 14:39:12 +0300

    update 64

--- assets/js/components/site-share-buttons.js
+++ assets/js/components/site-share-buttons.js
@@ -18,8 +18,8 @@ class SiteShareButtons extends HTMLElement {
           <a href="https://www.facebook.com/sharer/sharer.php?u=${url}" target="_blank" rel="noopener" class="ct-share-btn facebook" title="مشاركة عبر فيسبوك">
             <i class="fa fa-facebook"></i>
           </a>
-          <a href="https://twitter.com/intent/tweet?text=${title}&url=${url}" target="_blank" rel="noopener" class="ct-share-btn twitter" title="مشاركة عبر تويتر">
-            <i class="fa fa-twitter"></i>
+          <a href="https://x.com/intent/tweet?text=${title}&url=${url}" target="_blank" rel="noopener" class="ct-share-btn twitter" title="مشاركة عبر X">
+            <svg style="width: 18px; height: 18px; fill: currentColor;" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
           </a>
           <a href="https://t.me/share/url?url=${url}&text=${title}" target="_blank" rel="noopener" class="ct-share-btn telegram" title="مشاركة عبر تليجرام">
             <i class="fa fa-paper-plane"></i>
