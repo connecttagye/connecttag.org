b4769080a0323731acb32e809bcba2be955bfef6
‪Aqel Mohammed‬‏ <aqelmoh77@gmail.com>
2026-08-17 18:29:28 +0300

    update 152

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const ENABLE_CACHE = true;
-const CACHE_NAME = 'connecttag-cache-v59';
+const CACHE_NAME = 'connecttag-cache-v60';
 const OFFLINE_URL = 'https://connecttag.org/offline';
 
 const ASSETS_TO_CACHE = [
